d528ece563f3e8a2baa8ddbff0a75f3160b9b7fc
Shoaib Shaikh <shoshaikh7@gmail.com>
2022-04-12 09:53:51 -0700

    added form validation and submission

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ const navLinks = navBar.querySelectorAll('li');
 const navToggle = document.querySelector('#nav-toggle');
 const navToggleIcon = document.querySelector('#nav-toggle-icon');
 const logo = document.querySelector('#logo');
+const contactForm = document.querySelector("#contact-form");
+const formName = document.querySelector('#form-name input');
+const formEmail = document.querySelector('#form-email input');
+const formSubject = document.querySelector('#form-subject input');
+const formMessage = document.querySelector('#form-message textarea');
+const formSubmit = document.querySelector('#form-submit')
+const errorMessages = document.querySelectorAll('.error');
+const successMessage = document.querySelector('#success')
 
 // Toggle nav
 const showNav = () => {
@@ -36,7 +44,6 @@ document.addEventListener('click', (e) => {
   }
 });
 
-
 // Highlight active link on nav
 navLinks.forEach(link => {
   link.addEventListener('click', function () {
@@ -65,3 +72,55 @@ document.addEventListener('scroll', () => {
     navToggleIcon.style.fontSize = ('2rem');
   }
 });
+
+// Validate form data
+const validateForm = (e) => {
+  clearMessage();
+  let errorFlag = false;
+  if (formName.value.length < 1) {
+    errorMessages[0].innerText = 'Please enter your name';
+    formName.classList.add('error-border');
+    errorFlag = true;
+  }
+  if (!emailIsValid(formEmail.value)) {
+    errorMessages[1].innerText = 'Please enter a valid email';
+    formEmail.classList.add('error-border');
+    errorFlag = true;
+  }
+  if (formSubject.value.length < 1) {
+    errorMessages[2].innerText = 'Please enter a subject';
+    formSubject.classList.add('error-border');
+    errorFlag = true;
+  }
+  if (formMessage.value.length < 1) {
+    errorMessages[3].innerText = 'Please enter your message';
+    formMessage.classList.add('error-border');
+    errorFlag = true;
+  }
+  if (!errorFlag) {
+    contactForm.style.display = 'none';
+    successMessage.style.display = 'block';
+    successMessage.innerText = 'Message Sent! Thanks for reaching out, I\'ll get back to you as soon as I can.'
+    setTimeout(() => {
+      contactForm.submit();
+    }, 3000);
+  }
+}
+
+// Check if email is valid
+const emailIsValid = (email) => {
+  let pattern = /\S+@\S+\.\S+/;
+  return pattern.test(email);
+}
+
+// Clear error/success message
+const clearMessage = () => {
+  for (let i = 0; i < errorMessages.length; i++) {
+    errorMessages[i].innerText = '';
+  }
+  successMessage.innerText = '';
+  formName.classList.remove('error-border');
+  formEmail.classList.remove('error-border');
+  formSubject.classList.remove('error-border');
+  formMessage.classList.remove('error-border');
+}
